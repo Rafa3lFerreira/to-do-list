286c593b1285b1afbcdefc960adae5b652d5ca43
Rafael Ferreira <124806105+Rafa3lFerreira@users.noreply.github.com>
2025-07-03 15:19:49 -0300

    Feat: Adição de nova mensagem de boas vindas de acordo com o horario atual

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Bem vindo, {{ getUsuario() }}</h1>
+        <h1>{{ saudacao }}, {{ getUsuario() }}</h1>
         <hr>
         <div class="chart-container">
             <BarChart />
@@ -11,8 +11,22 @@
 <script setup>
 import { getUsuario } from '../main';
 import BarChart from '../components/InteractionChart.vue'
+import { computed } from 'vue';
 
-
+const saudacao = computed(() => {
+    const dateTime = new Date();
+    const horas = dateTime.toLocaleTimeString('pt-BR');
+ 
+    if (horas < '06:00:00') { // antes de 6:30
+        return 'Boa noite';
+    } else if (horas < '12:00:00') { // antes de 12:00
+        return 'Bom dia';
+    } else if (horas < '18:00:00') { // antes de 18:00
+        return 'Boa tarde';
+    } else {
+        return 'Boa noite';
+    }
+});
 </script>
 
 <style scoped>
